ee2d4581bda482a1312adcc499bc496c0719c529
Aditya Kumar Jha <mail@adijha.com>
2021-08-08 18:14:08 +0545

    diff-betweenjson-and-go-dsa

--- main.go
+++ main.go
@@ -11,13 +11,20 @@ type person struct {
 }
 
 func main() {
-	p1 := person{First: "Jenny"}
-	p2 := person{First: "James"}
+	p1, p2 := person{First: "Jenny"}, person{First: "James"}
+
 	xp := []person{p1, p2}
+
 	bs, err := json.Marshal(xp)
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(string(bs))
+	fmt.Println("JSON PRINT", string(bs))
+	xp2 := []person{}
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("back into go data structure", xp2)
 
 }
